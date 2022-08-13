45b471cbe1b21aeefee16b8b71abc377fbde3896
Angela Monsalve <angelapmonsalve@gmail.com>
2022-08-12 19:02:53 -0500

    Configuración de propiedades en .yml

--- src/main/java/com/angeladev/todoapi/context/SwaggerConfig.java
+++ src/main/java/com/angeladev/todoapi/context/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.angeladev.todoapi;
+package com.angeladev.todoapi.context;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

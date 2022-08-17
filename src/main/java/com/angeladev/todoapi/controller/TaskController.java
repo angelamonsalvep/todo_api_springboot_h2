c8ee84536d65bedc179bc9f57119223e446d3e88
Angela Monsalve <angelapmonsalve@gmail.com>
2022-08-17 16:28:37 -0500

    Se crea controlador para Task

--- src/main/java/com/angeladev/todoapi/controller/TaskController.java
+++ src/main/java/com/angeladev/todoapi/controller/TaskController.java
@@ -1,9 +1,25 @@
 package com.angeladev.todoapi.controller;
 
+import com.angeladev.todoapi.persistence.entity.Task;
+import com.angeladev.todoapi.service.TaskService;
+import com.angeladev.todoapi.service.dto.TaskInDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
 
+    private final TaskService taskService;
 
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody TaskInDTO taskInDTO){
+        return this.taskService.createTask(taskInDTO);
+    }
 }
